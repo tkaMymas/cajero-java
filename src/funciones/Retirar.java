70b3fb4a45f51c075de36c4fcf2598ecda3fa683
tkaMymas <JuanesvanegasGallego@gmail.com>
2023-08-01 18:34:14 -0500

    Ver si tienes suficiente dinero para retirar

--- src/funciones/Retirar.java
+++ src/funciones/Retirar.java
@@ -7,8 +7,13 @@ public class Retirar {
         System.out.println("\n¡Retirar!");
         System.out.print("Ingreso: ");
         int dineroRet = scInput.nextInt();
-        Menu.dineroBanco = Menu.dineroBanco - dineroRet;
-        System.out.println("Se retiro la cantidad de " +  dineroRet + "$\n");
+        if (Menu.dineroBanco - dineroRet < 0) {
+            System.out.println("No cuentas con suficiente dinero para realizar esta operación.\n");
+        }
+        else{
+            Menu.dineroBanco = Menu.dineroBanco - dineroRet;
+            System.out.println("Se retiro la cantidad de " +  dineroRet + "$\n");
+        }
         Menu.MetodoMenu();
     }
 }
