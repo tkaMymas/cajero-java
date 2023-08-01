a54ec01ca34ff06314d372aded1b4d2481e324b3
tkaMymas <JuanesvanegasGallego@gmail.com>
2023-08-01 18:45:20 -0500

    Revisar que los ingresos sean adecuados

--- src/funciones/Depositar.java
+++ src/funciones/Depositar.java
@@ -6,9 +6,14 @@ public class Depositar {
         Scanner scInput = new Scanner(System.in);
         System.out.println("\n¡Depositar!");
         System.out.print("Ingreso: ");
-        int dineroDep = scInput.nextInt();
-        Menu.dineroBanco += dineroDep;
-        System.out.println("Se ingreso la cantidad de " + dineroDep + "$\n");
+        try {
+            int dineroDep = scInput.nextInt();
+            Menu.dineroBanco += dineroDep;
+            System.out.println("Se ingreso la cantidad de " + dineroDep + "$\n");
+        }
+        catch (Exception e){
+            System.out.println("¡Error! Ingrese un valor numérico.\n");
+        }
         Menu.MetodoMenu();
     }
 }
--- src/funciones/Retirar.java
+++ src/funciones/Retirar.java
@@ -6,13 +6,18 @@ public class Retirar {
         Scanner scInput = new Scanner(System.in);
         System.out.println("\n¡Retirar!");
         System.out.print("Ingreso: ");
-        int dineroRet = scInput.nextInt();
-        if (Menu.dineroBanco - dineroRet < 0) {
-            System.out.println("No cuentas con suficiente dinero para realizar esta operación.\n");
+        try {
+            int dineroRet = scInput.nextInt();
+            if (Menu.dineroBanco - dineroRet < 0) {
+                System.out.println("No cuentas con suficiente dinero para realizar esta operación.\n");
+            }
+            else{
+                Menu.dineroBanco = Menu.dineroBanco - dineroRet;
+                System.out.println("Se retiro la cantidad de " +  dineroRet + "$\n");
+            }
         }
-        else{
-            Menu.dineroBanco = Menu.dineroBanco - dineroRet;
-            System.out.println("Se retiro la cantidad de " +  dineroRet + "$\n");
+        catch (Exception e){
+            System.out.println("¡Error! Ingrese un valor numérico.\n");
         }
         Menu.MetodoMenu();
     }
