98274ec0ecf2ec1cdc506856d6602ab0aa16130d
tkaMymas <JuanesvanegasGallego@gmail.com>
2023-08-05 10:58:10 -0500

    v1.6
    CorrecÂión de la funciÂón retirar

--- src/funciones/Retirar.java
+++ src/funciones/Retirar.java
@@ -12,7 +12,7 @@ public class Retirar {
                 System.out.println("No cuentas con suficiente dinero para realizar esta operación.\n");
             }
             else{
-                Menu.dineroBanco = Menu.dineroBanco - dineroRet;
+                Menu.dineroBanco -= dineroRet;
                 System.out.println("Se retiro la cantidad de " +  dineroRet + "$\n");
             }
         }
